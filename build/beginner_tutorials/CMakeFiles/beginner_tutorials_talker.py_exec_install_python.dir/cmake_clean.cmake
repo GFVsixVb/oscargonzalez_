c91d4829205625b6fda1aa9eb740b98b8ff2bbe4
Oscar <samuelgf111@gmail.com>
2024-04-27 10:18:22 -0400

    cambio

---
+++ build/beginner_tutorials/CMakeFiles/beginner_tutorials_talker.py_exec_install_python.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "C:/catkin_ws/devel/lib/talker.lib"
+  "CMakeFiles/beginner_tutorials_talker.py_exec_install_python.dir/catkin_generated/add_python_executable/beginner_tutorials_talker.py_exec_install_python/talker.cpp.obj"
+  "catkin_generated/windows_wrappers/beginner_tutorials_talker.py_exec_install_python/talker.exe"
+  "catkin_generated/windows_wrappers/beginner_tutorials_talker.py_exec_install_python/talker.exe.manifest"
+  "catkin_generated/windows_wrappers/beginner_tutorials_talker.py_exec_install_python/talker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/beginner_tutorials_talker.py_exec_install_python.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
